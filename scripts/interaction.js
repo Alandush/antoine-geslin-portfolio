4aa6f63d0d528a9493d64bec653373f1f3981174
Alandush <78132608+Alandush@users.noreply.github.com>
2025-08-22 13:50:33 +0200

    Update interaction.js

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -5,36 +5,52 @@ const transitionBlack = document.querySelector("#js-black-transition");
 
 let transition = false;
 
+let retourAll = false;
+
 // Transition
 
 setTimeout(() => { // Au lancemenent de la page on lance l'animation pour voir le contenu
     transitionBlack.classList.toggle("top-minus-50"); 
 }, 250);
 
-window.addEventListener("pageshow", (e) => { // Quand l'animation se termine et qu'on a cliqué sur un lien qui change de page, on se dirige vers une nouvelle page
-    transitionBlack.classList.toggle("top-minus-50");
+window.addEventListener("pageshow", (e) => { 
+    if(retourAll) {
+        transitionBlack.classList.toggle("top-minus-50");
+        retourAll = false;
+    }
+    
 });
 
 transitionBlack.addEventListener("transitionend", (e) => { // Quand l'animation se termine et qu'on a cliqué sur un lien qui change de page, on se dirige vers une nouvelle page
     if (transition && (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('/')) && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'choix.html';
     } else if (transition && window.location.pathname.endsWith('choix.html') && normal == true && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'n-sections.html';
     } else if (transition && window.location.pathname.endsWith('choix.html') && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'bubble.html';
     } else if (transition && window.location.pathname.endsWith('bubble.html') && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'd-sections.html';
     } else if (transition && window.location.pathname.endsWith('mouse-game.html') && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'design.html'; 
     } else if (transition && window.location.pathname.endsWith('memory-game.html') && !logoBurgerBool) {
+        retourAll = true;
         window.location.href = 'audiovisuel.html'; 
     }  else if (transition && idBurger == 0) {
+        retourAll = true;
         window.location.href = 'index.html';
     }  else if (transition && idBurger == 1) {
+        retourAll = true;
         window.location.href = 'n-sections.html';
     }  else if (transition && idBurger == 2) {
+        retourAll = true;
         window.location.href = 'd-sections.html';
     }  else if (transition && idBurger == 3) {
+        retourAll = true;
         window.location.href = 'index.html#contact'; 
     }
 });
@@ -43,6 +59,7 @@ function transitionToChangePage() {
     transitionBlack.classList.remove("top-minus-50");
     transition = true; 
 }
+
 
 
 
